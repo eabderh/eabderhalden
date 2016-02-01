801a19ef3156c34ed2bdd59a3bf9922d9c9ffc67
eabderh <eabderh@clemson.edu>
2016-02-01 06:14:57 +0000

    added upward file searching to deploy.sh
    searches upwards for deploy config file.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
 
-destination=$(awk -F ":" '/destination/{ print $2 }' .deploy)
-fsource=$(awk -F ":" '/source/{ print $2 }' .deploy)
+
+while :; do
+	if [ "$PWD" = "/" ]; then
+		echo "no .deploy file"
+		exit 1
+	fi
+	if [ -e ".deploy" ]; then
+		break
+	fi
+	cd ..
+done
+
+
+
+destination=$(awk -F ":" '/destination/{ print $2 }' "$path.deploy")
+fsource=$(awk -F ":" '/source/{ print $2 }' "$path.deploy")
 
 
 aws s3 sync "$fsource" "s3://$destination"
