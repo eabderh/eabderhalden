3d7daee2294f537e1e65baf6cd70716ce0d54d81
eabderh <eabderh@clemson.edu>
2016-06-09 10:15:32 +0000

    created website structure and implemented sass

--- test/server.js
+++ test/server.js
@@ -1,31 +1,14 @@
 var express = require("express");
-var jade = require("jade");
-var lessMiddleware = require("less-middleware");
-//var engines = require("consolidate")
-
 var app = express();
 
-app.set("views", __dirname + "/public");
-app.use(lessMiddleware(__dirname + "/public", ["debug"]));
+//app.set("views", __dirname + "/public");
 app.use(express.static(__dirname + "/public"));
-app.engine("jade", jade.__express);
-
-//app.set("view engine", "jade");
-//app.engine("jade", engines.jade);
 
 app.get("/", function(request, response){
-	response.render("home.jade");
+	response.render("/public/index.html");
 	});
 
 app.listen(4000, function() {
 	console.log("listening on port 4000");
 	});
-
-//var http = require("http")
-//var server = http.createServer(app);
-//
-//app.set("port", (process.env.PORT || 5000));
-//
-//app.get
-//
 
