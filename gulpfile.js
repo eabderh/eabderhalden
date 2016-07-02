611561b3cee049742e3c2feb273fd6468d8f6e84
eabderh <eabderh@clemson.edu>
2016-07-02 10:16:22 +0000

    updated deployment methods
    made gulp more adaptable.
    added command line arguments package via npm for above gulp issue.
    changed deploy file to deploy main site.

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,14 @@ var plumber 	= require('gulp-plumber');
 var jade 		= require('gulp-jade');
 var sass 		= require('gulp-sass');
 var browserSync = require('browser-sync');
+var argv 		= require('yargs').argv;
+
+var root = argv.d + '/';
+var src_path = root + 'src/';
+var public_path = root + 'public/';
 
 
-var root = 'test/'
-var src_path = root + 'src';
-var dest_path = root + 'public';
+
 
 
 gulp.task('default', ['browserSync'], function() {
@@ -16,10 +19,10 @@ gulp.task('default', ['browserSync'], function() {
 
 gulp.task('browserSync', ['jade', 'sass'], function() {
 	browserSync.init({
-		server: "./test/public",
+		server: public_path,
 		port: 4000
 	});
-	gulp.watch(dest_path + "**/*.html")
+	gulp.watch(public_path + "**/*.html")
 		.on('change', browserSync.reload);
 });
 
@@ -27,14 +30,14 @@ gulp.task('jade', function() {
 	return gulp.src(src_path + '**/*.jade', {base: src_path})
 		.pipe(plumber())
 		.pipe(jade())
-		.pipe(gulp.dest(dest_path));
+		.pipe(gulp.dest(public_path));
 });
 
 gulp.task('sass', function () {
 	return gulp.src(src_path + '**/*.scss', {base: src_path})
 		.pipe(plumber())
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest(dest_path))
+		.pipe(gulp.dest(public_path))
 		.pipe(browserSync.stream());
 });
 
