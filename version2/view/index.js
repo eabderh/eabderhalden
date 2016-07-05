4ad40fe6c96bdb2ecf5f5c9cebccbb0705abb0a7
eabderh <eabderh@clemson.edu>
2016-07-05 16:13:57 +0000

    started adding elias page content/config

--- version2/view/index.js
+++ version2/view/index.js
@@ -1,42 +1,40 @@
 $(function(){
-	var page = $(".page");
+	var view = $(".view");
 	console.log("test");
 	$('.nav-button').click(function(){
-		if(page.hasClass('showindex')) {
-			page.removeClass("showindex").addClass("showcontent");
+		if(view.hasClass('showindex')) {
+			view.removeClass("showindex").addClass("showcontent");
 		}
-		else if(page.hasClass('showcontent')) {
-			page.removeClass("showcontent").addClass("showindex");
-		}
-	});
-	$(".nav-swipe").on("swipeleft", function(event) {
-		if(page.hasClass('showindex')) {
-			page.removeClass("showindex").addClass("showcontent");
+		else if(view.hasClass('showcontent')) {
+			view.removeClass("showcontent").addClass("showindex");
 		}
 	});
-	$(".nav-swipe").on("swiperight", function(event) {
-		if(page.hasClass('showcontent')) {
-			page.removeClass("showcontent").addClass("showindex");
-		}
-	});
-	//$(document).on('touchmove', function(event) {
-	//	event.preventDefault();
-	//});
+	$(".sidebar, .content")
+		.on("swipeleft", function(event) {
+			if(view.hasClass('showindex')) {
+				view.removeClass("showindex").addClass("showcontent");
+			}
+		})
+		.on("swiperight", function(event) {
+			if(view.hasClass('showcontent')) {
+				view.removeClass("showcontent").addClass("showindex");
+			}
+		});
 });
 
 
 /*
 $(document).on("pagecreate", ".view", function() {
-	var page = $(".page");
+	var view = $(".view");
 	console.log("test2");
 	$(".nav-swipe").on("swipeleft", function(event) {
-		if(page.hasClass('showindex')) {
-			page.removeClass("showindex").addClass("showcontent");
+		if(view.hasClass('showindex')) {
+			view.removeClass("showindex").addClass("showcontent");
 		}
 	});
 	$(".nav-swipe").on("swiperight", function(event) {
-		if(page.hasClass('showcontent')) {
-			page.removeClass("showcontent").addClass("showindex");
+		if(view.hasClass('showcontent')) {
+			view.removeClass("showcontent").addClass("showindex");
 		}
 	});
 });
